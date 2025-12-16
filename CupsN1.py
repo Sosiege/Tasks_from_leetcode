6b315946c49bd9d6953f74d3356033b9eda4b85f
Sosiege <edinorog599@gmail.com>
2025-12-16 10:15:03 +0300

    Add files via upload

---
+++ CupsN1.py
@@ -0,0 +1,18 @@
+n, X = map(int, input().split())
+boxes = list(map(int, input().split()))
+
+remainder_count = dict()
+current_sum = 0
+count = 0
+
+remainder_count[0] = 1
+
+for num in boxes:
+    current_sum = (current_sum + num) % X
+    
+    if current_sum in remainder_count:
+        count += remainder_count[current_sum]
+    
+    remainder_count[current_sum] = remainder_count.get(current_sum, 0) + 1
+
+print(count)
